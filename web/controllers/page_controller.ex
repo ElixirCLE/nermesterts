5befb622b01c19313539452dbf90c09d81977450
Dave Shah <david.a.shah@gmail.com>
2016-10-10 17:25:10 -0400

    Sloppy example for Chris

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,17 @@ defmodule Nermesterts.PageController do
   use Nermesterts.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", game: nil
+  end
+
+  def post(conn, %{"player_info" => info}) do
+    players = String.to_integer(info["number_of_players"])
+    case GamePicker.pick_game(GamePicker.games, players) do
+      %{name: name} ->
+        render conn, "index.html", game: name
+      _ ->
+        render conn, "index.html", game: nil
+    end
+
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Nermesterts.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    post "/", PageController, :post
   end
 
   # Other scopes may use custom stacks.
