1c2a13655594f10c13bebc22048013428dfc57bc
Dave Shah <david.a.shah@gmail.com>
2016-10-10 16:02:29 -0400

    TODO - move these into a mo' better spot

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule Nermesterts.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.create --quiet", "ecto.migrate", "test"],
+     "s": ["phoenix.server"]]
   end
 end
