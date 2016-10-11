a6351b3f0d8fd2298754adb9a693f83d98312005
Dave Shah <david.a.shah@gmail.com>
2016-10-11 17:12:55 -0400

    MVP'ing with Chris and Dave

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,20 +4,8 @@ defmodule Nermesterts.PageController do
 
   def index(conn, _params) do
     players = Nermesterts.Repo.all(Player)
-    render conn, "index.html", game: nil, players: players
-  end
-
-  def post(conn, %{"player_info" => info}) do
-    num_players = players(info)
+    num_players = length(players)
     game = GamePicker.pick_game(GamePicker.games, num_players)
-    players = Nermesterts.Repo.all(Player)
     render conn, "index.html", game: game, players: players
-  end
-
-  defp players(info) do
-    case Integer.parse(info["number_of_players"]) do
-      { players, _ } -> players
-        _ -> 0
-    end
   end
 end
