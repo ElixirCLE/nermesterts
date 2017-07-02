e4875e1bbc8a1c00ab35aae9115be41709e7f986
Chris Rees <crees@covermymeds.com>
2017-07-02 16:48:53 -0400

    Consider CMT to be a complete MVP. Release v1.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Nermesterts.Mixfile do
 
   def project do
     [app: :nermesterts,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
